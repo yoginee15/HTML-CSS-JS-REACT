45d1307274d3a005851ad0b39016fa242d842e7e
yoginee15 <yogineebalkawde15@gmail.com>
2021-12-09 21:13:51 +0530

    Adding javascript notes.txt

---
+++ Javascript/main.js
@@ -0,0 +1,18 @@
+alert("Hi")
+"use strict"
+let c = 10;
+console.log("Hello")
+var a = 5 // global variable : window.a
+alert(window.a +
+    3
+    + 0)
+
+// let a = alert("Hello")
+// [1, 2].forEach((num) => a)
+
+var a = 9
+function test() {
+    var b = 8
+    console.log(Array.isArray([1, 2]))
+}
+test()
