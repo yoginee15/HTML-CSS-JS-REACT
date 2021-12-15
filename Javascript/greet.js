2bdb7e7201c200bdc2705fe4b9c52e5ebf343722
yoginee15 <yogineebalkawde15@gmail.com>
2021-12-15 20:30:30 +0530

    Function expression, callback functions and arrow function concept

--- Javascript/greet.js
+++ Javascript/greet.js
@@ -1,2 +1,26 @@
-let user = prompt("Please enter your name")
-alert("Welcome " + user)
+// let user = prompt("Please enter your name")
+// alert("Welcome " + user)
+
+
+// function handleInputs(a, b, add, concat) {
+//     if (typeof a == "number" && typeof b == "number") {
+//         return add(a, b)
+//     } else {
+//         return concat(a, b)
+//     }
+// }
+
+// let add = function (a, b) {
+//     return "Concatenation is: " + a + b
+// }
+// let concat = function (a, b) {
+//     return "Concatenation is: " + a + b
+// }
+
+// console.log(handleInputs(2, 3,
+//     add,
+//     concat))
+// console.log(handleInputs("test", " string", addNumber, concat))
+
+let add = () => 2 + 3
+console.log(add())  //undefined
